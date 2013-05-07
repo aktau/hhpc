b782a91acbea8e9b1fcda77150a0ce877a241f2c
Aktau <nicolashillegeer@gmail.com>
2013-05-07 14:01:53 +0200

    compiles, runs and loops forever doing seemingly nothing except for timing out and not detecting anything, time to dig deeper...

--- hhpc.c
+++ hhpc.c
@@ -28,12 +28,61 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
 
+#include <sys/select.h>
+#include <sys/time.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-static void waitForMotion(Display *dpy, Window win) {
+static void waitForMotion(Display *dpy, Window win, int timeout) {
+	int ready = 0;
+	int xfd   = ConnectionNumber(dpy);
+	long mask = PointerMotionMask;
 
+	fd_set fds;
+	struct timeval tv;
+
+	XEvent event;
+
+	XSelectInput(dpy, win, mask);
+
+	/**
+	 * flush the commands we just sent because we're not going to use a
+	 * conventional XNextEvent-based loop (which would flush automatically)
+	 */
+	XFlush(dpy);
+
+	printf("flushed!\n");
+
+	while (1) {
+		/* add the X11 fd to the fdset so we can poll/select on it */
+		FD_ZERO(&fds);
+		FD_SET(xfd, &fds);
+
+		/* poll with a timeout */
+		tv.tv_usec = 0;
+		tv.tv_sec  = timeout;
+
+		ready = select(xfd + 1, &fds, NULL, NULL, &tv);
+
+		if (ready > 0) {
+			printf("event received\n");
+		}
+		else if (ready == 0) {
+			printf("timeout\n");
+		}
+		else {
+			perror("error while select()'ing");
+		}
+
+		/* drain events */
+		while (XPending(dpy)) {
+			XNextEvent(dpy, &event);
+
+			printf("-> could start processing event...\n");
+		}
+	}
 }
 
 int main(void) {
@@ -42,9 +91,9 @@ int main(void) {
 
   Window rootwin = RootWindow(dpy, scr);
 
-  printf("Got root window, screen = %d\n", scr);
+  printf("Got root window, screen = %d, display = %p, rootwin = %d\n", scr, (void *) dpy, (int) rootwin);
 
-  waitForMotion(dpy, rootwin);
+  waitForMotion(dpy, rootwin, 1);
 
   XCloseDisplay(dpy);
 
